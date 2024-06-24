44e42192b6631254c46136109db3b2882a4c644d
amr4356 <amrooo73321@gmail.com>
2024-06-25 02:34:17 +0300

    161. Managing Tasks & Understanding Prop Drilling

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,7 +1,7 @@
 import Tasks from "./Tasks";
 
 
-export default function SelectedProject({project,onDelete}){
+export default function SelectedProject({project,onDelete,onAddTask,onDeleteTask,tasks}){
     const formattedDate=new Date(project.dueDate).toLocaleDateString('en-US',{
         year:'numeric',
         month:'short',
@@ -21,7 +21,7 @@ export default function SelectedProject({project,onDelete}){
                 <p className="mb-4 text-stone-400">{formattedDate}</p>
                 <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
             </header>
-            <Tasks />
+            <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
         </div>
     );
 }
